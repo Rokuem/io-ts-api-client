8a0103846efe231384b0dc2ce4b5c224df6d7047
Mateus Amorim <mateusamorim2@gmail.coms>
2021-04-05 09:28:19 -0300

    Add t.schema

--- src/modules/t/t.ts
+++ src/modules/t/t.ts
@@ -4,5 +4,16 @@ import * as extraTypes from 'io-ts-types';
 export const t = {
   ...types,
   ...extraTypes,
+  /**
+   * Creates null intersection with provided type.
+   */
   nullable: <T extends types.Any>(type: T) => types.union([t.null, type]),
+  /**
+   * Creates intersection between required and optional props.
+   * @since 0.7.4
+   */
+  schema: <T extends types.Props, P extends types.Props>(type: { required: T, optional: P }) => types.intersection([
+    types.interface(type.required),
+    types.partial(type.optional)
+  ])
 };
